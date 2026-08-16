a997579f383b3f65158e9d48c23752c08317a34f
Amanda Pereira <amandiholiv@gmail.com>
2026-08-16 18:28:35 -0300

    Adicionando exercicios java

---
+++ src/src/Exercicio1.java
@@ -0,0 +1,18 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+
+        String nome = "Maria de Souza";
+        int idade = 28;
+        int codigo = 1234;
+        char sexo = 'F';
+
+        System.out.println("*** Dados do(a) comprador(a) ***");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Código: " + codigo);
+        System.out.println("Sexo: " + sexo);
+
+        System.out.println("*** Lista de Produtos ***");
+
+    }
+}
